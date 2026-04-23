a1482981b24d7d8400aae228970e1e0df968e26d
Egor Laptev <egor.laptev91@yandex.ru>
2026-04-23 17:15:02 +0300

    24.2 Kanban part 2

--- src/main/java/ru/kanban/Main.java
+++ src/main/java/ru/kanban/Main.java
@@ -1,38 +1,49 @@
 package ru.kanban;
 
+import ru.kanban.manager.InMemoryTaskManager;
 import ru.kanban.manager.TaskManager;
-import ru.kanban.model.Epic;
-import ru.kanban.model.SimpleTask;
-import ru.kanban.model.Status;
-import ru.kanban.model.Subtask;
+import ru.kanban.model.*;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        TaskManager manager = new TaskManager();
+        TaskManager manager = new InMemoryTaskManager();
 
-        SimpleTask task1 = manager.createSimpleTask(new SimpleTask("Задача 1", "Описание задачи 1", Status.NEW));
-        SimpleTask task2 = manager.createSimpleTask(new SimpleTask("Задача 2", "Описание задачи 2", Status.IN_PROGRESS));
+        SimpleTask task1 = manager.createSimpleTask(new SimpleTask("Задача 1", "Купить продукты", Status.NEW));
+        SimpleTask task2 = manager.createSimpleTask(new SimpleTask("Задача 2", "Помыть окна", Status.NEW));
 
-        Epic epic1 = manager.createEpic(new Epic("Эпик 1", "Описание эпика 1", Status.NEW));
-        Subtask subtask1 = manager.createSubtask(new Subtask("Подзадача 1", "Описание подзадачи 1", Status.NEW, epic1.getId()));
-        Subtask subtask2 = manager.createSubtask(new Subtask("Подзадача 2", "Описание подзадачи 2", Status.NEW, epic1.getId()));
+        Epic epic1 = manager.createEpic(new Epic("Эпик 1", "Переезд", Status.NEW));
+        Subtask sub1 = manager.createSubtask(new Subtask("Подзадача 1", "Собрать вещи", Status.NEW, epic1.getId()));
+        Subtask sub2 = manager.createSubtask(new Subtask("Подзадача 2", "Заказать машину", Status.NEW, epic1.getId()));
 
-        epic1.addSubtaskId(subtask1.getId());
-        epic1.addSubtaskId(subtask2.getId());
+        manager.getSimpleTaskById(task1.getId());
+        manager.getSimpleTaskById(task2.getId());
+        manager.getEpicById(epic1.getId());
+        manager.getSubtaskById(sub1.getId());
+        manager.getSubtaskById(sub2.getId());
+        manager.getSimpleTaskById(task1.getId());
+        manager.getSimpleTaskById(task1.getId());
+        manager.getSimpleTaskById(task2.getId());
+        manager.getEpicById(epic1.getId());
+        manager.getSubtaskById(sub1.getId());
+        manager.getSubtaskById(sub2.getId());
+        manager.getSimpleTaskById(task1.getId());
 
-        System.out.println("Все простые задачи:");
-        manager.getAllSimpleTasks().forEach(t -> System.out.println(t.getName() + " - " + t.getStatus()));
+        List<Task> history = manager.getHistory();
+        System.out.println("Количество элементов в истории: " + history.size());
 
-        System.out.println("\nВсе эпики:");
-        manager.getAllEpics().forEach(e -> System.out.println(e.getName() + " - " + e.getStatus()));
+        for (int i = 0; i < history.size(); i++) {
+            System.out.println((i + 1) + ". " + history.get(i).getName());
+        }
 
-        System.out.println("\nПодзадачи эпика " + epic1.getName() + ":");
-        manager.getSubtasksByEpicId(epic1.getId()).forEach(s -> System.out.println(s.getName() + " - " + s.getStatus()));
+        System.out.println("Статус до завершения подзадач: " + epic1.getStatus());
 
-        subtask1.setStatus(Status.DONE);
-        manager.updateSubtask(subtask1);
+        sub1.setStatus(Status.DONE);
+        sub2.setStatus(Status.DONE);
+        manager.updateSubtask(sub1);
+        manager.updateSubtask(sub2);
 
-        System.out.println("\nПосле обновления статуса подзадачи:");
-        System.out.println("Эпик: " + epic1.getName() + " - " + epic1.getStatus());
+        System.out.println("Статус после завершения всех подзадач: " + epic1.getStatus());
     }
 }
