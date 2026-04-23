b735fd750a1375afb894470fb713a25c735d7aca
Egor Laptev <egor.laptev91@yandex.ru>
2026-04-23 16:38:29 +0300

    24.1 Kanban part 1

---
+++ src/main/java/ru/kanban/model/Task.java
@@ -0,0 +1,46 @@
+package ru.kanban.model;
+
+public abstract class Task {
+    private String name;
+    private String description;
+    private int id;
+    private Status status;
+
+    public Task(String name, String description, Status status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
